ada9f206fb647eac773085e9eb00b91baaf2bdab
Tisha <sumaiyataslim43@gmail.com>
2025-11-09 00:20:07 +0600

    added js file for button

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ document.getElementById("loginButton").addEventListener("click", function (e) {
   e.preventDefault();
   console.log("login button clicked");
   console.log(e);
+  const mobileNumber = 11316668115;
+  const pinNumber = 1234;
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  console.log(mobileNumberValueConverted);
+  console.log(pinNumberValueConverted);
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    // console.log("all values matched ");
+    window.location.href = "./main.html";
+  } else {
+    // console.log("Invalid");
+    alert("Invalid");
+  }
 });
