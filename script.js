afac38f179616c0a62e9b289eaadd023aedb2224
Tisha <sumaiyataslim43@gmail.com>
2025-11-08 22:05:07 +0600

    added event listener

---
+++ script.js
@@ -0,0 +1,6 @@
+// login button functionality
+document.getElementById("loginButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("login button clicked");
+  console.log(e);
+});
